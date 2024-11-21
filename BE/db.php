f0975c00648626842422ce66f787f6c1af7f68fa
erickcodessoftware <e1291222@uabc.edu.mx>
2024-11-20 20:34:35 -0800

    Uptade database connection

--- BE/db.php
+++ BE/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";  // Cambiar al servidor de InfinityFree
-$username = "root";     // Tu nombre de usuario de FTP
-$password = "8qqs6kg1AAAwasi";    // Tu contraseña de FTP
-$dbname = "hot_pizzas";          // Nombre de la base de datos en InfinityFree
+$servername = "localhost"; // Cambiar al servidor de InfinityFree
+$username = "root";        // Tu nombre de usuario de FTP
+$password = "1234";        // Tu contraseña de FTP
+$dbname = "hot_pizzas";    // Nombre de la base de datos en InfinityFree
 
 // Conectar a la base de datos en InfinityFree
 $conn = new mysqli($servername, $username, $password, $dbname);

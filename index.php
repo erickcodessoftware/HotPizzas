d71eb2602013fdd1b292e2c00d57f964813cf471
erickcodessoftware <e1291222@uabc.edu.mx>
2024-11-20 21:42:37 -0800

    Add explicative video

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,14 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservaciones de Restaurantes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="bg-light">
     <div class="container text-center mt-5">
         <h1 class="display-4 text-primary mb-4">Bienvenido a Hot Pizzas</h1>
         
-        <div class="d-grid gap-2 col-6 mx-auto">
+        <div class="d-grid gap-2 col-6 mx-auto mb-4">
             <button onclick="window.location.href='admin.php'" class="btn btn-success btn-lg">Administrador</button>
             <button onclick="window.location.href='user.php'" class="btn btn-warning btn-lg">Usuario</button>
+        </div>
+
+        <!-- Video de YouTube -->
+        <div class="video-container ratio ratio-16x9">
+            <iframe src="https://www.youtube.com/embed/9s8DSu1ThwI" 
+                    title="Video de YouTube" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+            </iframe>
         </div>
     </div>
 
